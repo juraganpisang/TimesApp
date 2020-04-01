a49bc7dde10de7da498c8e49ce61630362f2b518
juraganpisang <ronaldarrivalf@gmail.com>
2020-04-01 11:36:53 +0700

    update settings

--- app/src/main/java/com/jrg/pisang/timesapp/SettingFragment.java
+++ app/src/main/java/com/jrg/pisang/timesapp/SettingFragment.java
@@ -25,7 +25,8 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        return view;
     }
 
 }
